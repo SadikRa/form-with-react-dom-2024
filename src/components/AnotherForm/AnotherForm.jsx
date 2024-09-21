1f5063a1306733420535171300a1a14fd1e3ce9c
SadikRa <sadikrahman494@gmail.com>
2024-09-21 21:57:30 +0600

    get data done

--- src/components/AnotherForm/AnotherForm.jsx
+++ src/components/AnotherForm/AnotherForm.jsx
@@ -1,45 +1,76 @@
+import { useState } from "react";
+
 /* eslint-disable react/no-unknown-property */
 const AnotherForm = () => {
+  const [formData, setFormData] = useState({ name: "", email: "", password: "" });
+
+  console.log(formData);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const data = { name, email, password };
+    setFormData(data);
+  };
+
   return (
     <div>
-      <div class="hero bg-base-200 min-h-screen">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form class="card-body">
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  class="input input-bordered"
-                  required
-                />
+      <form onSubmit={handleSubmit} action="">
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <input className="btn btn-primary" type="submit" value="Submit" />
+                </div>
               </div>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  class="input input-bordered"
-                  required
-                />
-                <label class="label">
-                  <a href="#" class="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div class="form-control mt-6">
-                <button class="btn btn-primary">Login</button>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
